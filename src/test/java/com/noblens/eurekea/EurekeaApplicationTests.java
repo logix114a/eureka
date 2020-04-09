82b6bd74f89d8b14185c26db378bf8249a884c29
odenoblens <odenoblens@manh.com>
2020-04-09 15:21:17 +0200

    move directory

---
+++ src/test/java/com/noblens/eurekea/EurekeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noblens.eurekea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
